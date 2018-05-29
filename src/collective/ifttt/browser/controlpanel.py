bb7de9a52a6146470793cae835f6a6d5f8f80bbd
Shriyanshagro <agrawalshriyansh05@gmail.com>
2018-05-29 15:55:13 +0530

    language translatable

--- src/collective/ifttt/browser/controlpanel.py
+++ src/collective/ifttt/browser/controlpanel.py
@@ -1,26 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from collective.ifttt import _
+from collective.ifttt.interfaces import IIftttControlPanel
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
 from plone.z3cform import layout
-from zope import schema
-from zope.interface import Interface
-
-
-class IIftttControlPanel(Interface):
-
-    ifttt_secret_key = schema.TextLine(
-        title=_(u'Secret Key'),
-        description=_(u'Register Ifttt"s secret key'),
-        required=False,
-    )
 
 
 class IftttControlPanelForm(RegistryEditForm):
     schema = IIftttControlPanel
     schema_prefix = 'ifttt'
-    label = u'Ifttt Settings'
+    label = _(u'Ifttt Settings')
 
 
 IftttControlPanelView = layout.wrap_form(
--- src/collective/ifttt/interfaces.py
+++ src/collective/ifttt/interfaces.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from collective.ifttt import _
+from plone.autoform import directives
+from zope import schema
+from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class ICollectiveIftttLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IIftttControlPanel(Interface):
+
+    # A field in 'hidden' mode
+
+    directives.mode(secret='hidden')
+    ifttt_secret_key = schema.TextLine(
+        title=_(u'Secret Key'),
+        description=_(u'Register Ifttt"s secret key'),
+    )
