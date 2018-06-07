d0fdb443fbf8de2b37f9abe366ac23c0df0cbf96
Shriyanshagro <agrawalshriyansh05@gmail.com>
2018-06-08 03:04:26 +0530

    send data to ifttt

--- src/collective/ifttt/actions/ifttt.py
+++ src/collective/ifttt/actions/ifttt.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from concurrent.futures import ThreadPoolExecutor
 from OFS.SimpleItem import SimpleItem
+from plone import api
 from plone.app.contentrules import PloneMessageFactory as _
 from plone.app.contentrules.actions import ActionAddForm
 from plone.app.contentrules.actions import ActionEditForm
@@ -15,16 +16,19 @@ from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
 
 import logging
+import requests
 
 
 logger = logging.getLogger('collective.ifttt')
 
+PAYLOAD_DESCRIPTION = 'description'
+PAYLOAD_USERNAME = 'username'
+PAYLOAD_START = 'start'
+
 payload_options = SimpleVocabulary([
-    SimpleTerm(value=u'Description', title=_(u'Description of action')),
-    SimpleTerm(value=u'Username', title=_(u'Username of Editor')),
-    SimpleTerm(
-        value=u'Event start date/time', title=_(u'Event Start Date/Time')
-    ),
+    SimpleTerm(value=PAYLOAD_DESCRIPTION, title=_(u'Description of action')),
+    SimpleTerm(value=PAYLOAD_USERNAME, title=_(u'Username of Editor')),
+    SimpleTerm(value=PAYLOAD_START, title=_(u'Event Start Date/Time'))
 ])
 
 
@@ -32,24 +36,26 @@ class IIftttTriggerAction(Interface):
     """
         Definition of the configuration available for a  Ifttt action
     """
-    ifttt_name = schema.TextLine(
-        title=_(u'IFTTT event name'),
-        description=_(u'The IFTTT event name'),
-        required=False,
+    Title = schema.TextLine(
+        title=_(u'Title'),
+        description=_(u'Title/Subject for action'),
+        required=True,
+    )
+
+    URL = schema.URI(
+        title=_(u'URL'),
+        description=_(u'For files/images to view'),
+        required=True,
     )
 
     payload_option = schema.Choice(
-        title=_(u'Choose 2nd Payload'),
+        title=_(u'Choose 3rd Payload'),
         vocabulary=payload_options,
         description=_(
-            u'Choose whether the 3rd payload is Description, '
+            u'Choose whether the 3rd payload is Description of Content rule, '
             u'Username of editor, or Event Start Date/time'
         ),
-    )
-    payload = schema.Text(
-        title=_(u'Payload'),
-        required=False,
-        description=_(u'Provide input for chosen payload')
+        required=True,
     )
 
 
@@ -59,25 +65,22 @@ class IftttTriggerAction(SimpleItem):
         The implementation of the action defined before
     """
 
-    ifttt_name = u''
+    Title = u''
+    URL = u''
     payload_option = u''
-    payload = u''
 
     element = 'plone.actions.Ifttt'
 
     @property
     def summary(self):
         return _(
-            u'${ifttt_name} ${payload_option}',
+            u'${Title} ${payload_option}',
             mapping=dict(
-                ifttt_name=self.ifttt_name,
+                Title=self.Title,
+                URL=self.URL,
                 payload_option=self.payload_option,
-                payload=self.payload,
             ),
         )
-
-
-EXECUTOR = ThreadPoolExecutor(max_workers=1)
 
 
 @implementer(IExecutable)
@@ -88,25 +91,35 @@ class IftttTriggerActionExecutor(object):
     """
     timeout = 120
 
-    def __init__(self, element):
+    def __init__(self, element, context, event):
         self.element = element
+        self.context = context
+        self.event = event
 
     def __call__(self, *args, **kwargs):
-        r = self.element.request
-        ifttt_name = self.element.ifttt_name
+        Title = self.element.Title
+        URL = self.element.URL
         payload_option = self.element.payload_option
-        payload = self.element.payload
+        # TODO explore context and event; element is defined with Ifttt Trigger form
+        # TODO define ifttt_url with secret key
+        ifttt_name = self.event.title
+        secret_key = self.event.secret_key
+        ifttt_url = 'https://maker.ifttt.com/trigger/' + ifttt_name + '/with/key/' + secret_key
+        # TODO define payload value
+        payload = {'value1': Title, 'value2': URL, 'value3': ''}
+        if payload_option == PAYLOAD_DESCRIPTION:
+            payload['value3'] = self.event.description
+        elif payload_option == PAYLOAD_USERNAME:
+            payload['value3'] = api.user.get_users()
+        elif payload_option == PAYLOAD_START:
+            payload['value3'] = self.context.start
+        logger.info('Calling Post request to Ifttt')
         try:
-            EXECUTOR.submit(
-                # Post HTTP request
-                r.post,
-                ifttt_name,
-                payload_option,
-                data=payload,
-                timeout=self.timeout
-            )
-        except TypeError:
-            logger.exception('Error calling Ifttt Trigger:')
+            # Post HTTP request
+            requests.post(ifttt_url, data=payload, timeout=self.timeout)
+            logger.info('Successful request call to Ifttt')
+        except:
+            logger.exception('Error calling Ifttt Trigger')
         return True
 
 
@@ -117,8 +130,7 @@ class IftttAddForm(ActionAddForm):
     schema = IIftttTriggerAction
     label = _(u'Add Ifttt Trigger Action')
     description = _(
-        u'An ifttt trigger action will execute HTTP POST with '
-        u'interpolated  JSON payload.'
+        u'An ifttt trigger action will execute POST request to Ifttt'
     )
     form_name = _(u'Configure element')
     Type = IftttTriggerAction
@@ -136,8 +148,7 @@ class IftttEditForm(ActionEditForm):
     schema = IIftttTriggerAction
     label = _(u'Edit Ifttt Action')
     description = _(
-        u'An ifttt trigger action will execute HTTP POST with '
-        u'interpolated  JSON payload.'
+        u'An ifttt trigger action will execute POST request to Ifttt'
     )
     form_name = _(u'Configure element')
 
