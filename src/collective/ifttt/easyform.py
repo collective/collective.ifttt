ccb13418ef2e7ac423f12c54d601d366ab6819d9
ShriyanshAgro <agrawalshriyansh05@gmail.com>
2018-08-03 17:00:21 +0530

    Ifttt trigger executor for easyforms

--- src/collective/ifttt/easyform.py
+++ src/collective/ifttt/easyform.py
@@ -4,9 +4,19 @@ from collective.easyform.actions import Action
 from collective.easyform.actions import ActionFactory
 from collective.easyform.interfaces import IAction
 from collective.ifttt import _
+from collective.ifttt.interfaces import IRequestsLibrary
+from plone import api
 from plone.supermodel.exportimport import BaseHandler
 from zope import schema
+from zope.component import queryMultiAdapter
+from zope.globalrequest import getRequest
 from zope.interface import implementer
+
+import logging
+import requests
+
+
+logger = logging.getLogger('collective.ifttt')
 
 
 class IIFTTT(IAction):
@@ -43,9 +53,49 @@ class IFTTTTrigger(Action):
             setattr(self, i, kw.pop(i, f.default))
         super(IFTTTTrigger, self).__init__(**kw)
 
+    # send field data to IFTTT event and Execute IFTTTTrigger Action
     def onSuccess(self, fields, request):
-        # send field data to IFTTT event and Execute IFTTTTrigger Action
-        pass
+
+        payload = {}
+        # get fields data as payload data
+        for i in self.payload_fields:
+            payload[i] = fields.get(i)
+
+        # send payload data to IFTTTActionExecutor
+        self.IftttTriggerActionExecutor(payload)
+
+    def IftttTriggerActionExecutor(self, payload):
+        """
+            The IFTTT Trigger executor for easyforms
+        """
+        timeout = 120
+        secret_key = api.portal.get_registry_record('ifttt.ifttt_secret_key')
+        ifttt_trigger_url = 'https://maker.ifttt.com/trigger/' + \
+                            self.ifttt_event_name + '/with/key/' + secret_key
+
+        r = queryMultiAdapter((self, getRequest()),
+                              IRequestsLibrary,
+                              default=requests)
+
+        logger.info('Calling Post request to IFTTT')
+        try:
+
+            r.post(ifttt_trigger_url, data=payload, timeout=timeout)
+
+            logger.info(
+                'Successful Post request to IFTTT applet {0}'.format(
+                    self.ifttt_event_name
+                )
+            )
+
+        except TypeError:
+            logger.exception(
+                'Error calling IFTTT Trigger {0}'.format(
+                    self.ifttt_event_name
+                )
+            )
+
+        return True
 
 
 IFTTTAction = ActionFactory(
